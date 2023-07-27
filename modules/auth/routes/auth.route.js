a53027a760e22a8e2df1fb302cc4e57c7ab67954
Khalid Utsob <en.arnob@gmail.com>
2023-07-27 14:37:58 +0600

    OTP Verification moved to backend

--- modules/auth/routes/auth.route.js
+++ modules/auth/routes/auth.route.js
@@ -25,7 +25,7 @@ module.exports = function (app) {
 
   // verify otp for reg completion
   app.put(
-    "/api/user/verify-otp-put/:mobile",
+    "/api/user/verify-otp-put/:mobile/:otp",
     registrationController.confirmOTP
   );
 };
