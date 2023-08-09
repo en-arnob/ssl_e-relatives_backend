a2e3a84905ce3752c67bd95a5bbeff3d150f1d84
Khalid Utsob <en.arnob@gmail.com>
2023-08-09 14:39:22 +0600

    RequestByMe all APIs completed

--- modules/serviceRequests/reqByMe/controllers/reqByMe.controller.js
+++ modules/serviceRequests/reqByMe/controllers/reqByMe.controller.js
@@ -7,6 +7,7 @@ const State = db.model.state;
 const Country = db.model.country;
 const errorResponse = require("../../../../utils/errorResponse");
 const successResponse = require("../../../../utils/successResponse");
+const { Op } = require("sequelize");
 
 exports.getAll = async (req, res) => {
   const { userId } = req.params;
@@ -66,6 +67,40 @@ exports.getAll = async (req, res) => {
     });
     if (myReqs) {
       successResponse(200, "OK", myReqs, res);
+    }
+  } catch (err) {
+    errorResponse(
+      500,
+      "ERROR",
+      err.message || "Some error occurred while Finding data",
+      res
+    );
+  }
+};
+
+exports.getDonors = async (req, res) => {
+  const reqId = req.params.reqId;
+  const userId = req.params.userId;
+  try {
+    const reqWithDonors = await BloodRequest.findAll({
+      where: {
+        req_no: reqId,
+        user_id: userId,
+        accepted_donor: {
+          [Op.ne]: null,
+        },
+      },
+      include: [
+        {
+          model: User,
+          as: "donor",
+          attributes: ["id", "f_name", "mobile", "email", "image", "address_1"],
+        },
+      ],
+    });
+    console.log(reqWithDonors);
+    if (reqWithDonors) {
+      successResponse(200, "OK", reqWithDonors, res);
     }
   } catch (err) {
     errorResponse(
--- modules/serviceRequests/reqByMe/routes/reqByMe.route.js
+++ modules/serviceRequests/reqByMe/routes/reqByMe.route.js
@@ -3,6 +3,7 @@ const reqByMeController = require("../controllers/reqByMe.controller");
 
 module.exports = (app) => {
   reqByMeRouter.get("/:userId", reqByMeController.getAll);
+  reqByMeRouter.get("/donors/:reqId/:userId", reqByMeController.getDonors);
 
   app.use("/api/services/requests-by-me", reqByMeRouter);
 };
