d6923473c48e80d24f82bab263c7b4e70b97c0e0
Mizanur Rahman <mizanurewu@gmail.com>
2023-08-10 20:38:22 +0600

    coll point

--- modules/serviceRequests/collPointRequest/controllers/collPointReq.Controller.js
+++ modules/serviceRequests/collPointRequest/controllers/collPointReq.Controller.js
@@ -1,15 +1,25 @@
 const db = require("../../../../config/database.config");
 const BloodRequest = db.model.bloodReq;
+const User = db.model.user;
+const UserDetails = db.model.UserDetails;
+const City = db.model.city;
+const State = db.model.state;
+const Country = db.model.country;
 const errorResponse = require("../../../../utils/errorResponse");
 const successResponse = require("../../../../utils/successResponse");
+const { Op } = require("sequelize");
 
 exports.getAll = async (req, res) => {
   const { collectionPoint } = req.params;
-  
+  console.log(collectionPoint);
+
   try {
     const data = await BloodRequest.findAll({
       where: {
-        collection_point: 59,
+        collection_point: collectionPoint,
+        accepted_donor: {
+          [Op.not]: null,
+        },
       },
     });
 
@@ -17,7 +27,37 @@ exports.getAll = async (req, res) => {
       return errorResponse(404, "NOT_FOUND", "No data found", res);
     }
 
-    successResponse(200, "OK", data, res);
+    // Extract user_ids and accepted_donor values from the data
+    const userIds = data.map(item => item.user_id);
+    const donorIds = data.map(item => item.accepted_donor);
+
+    // Fetch user details for user_ids and accepted_donor values
+    const users = await User.findAll({
+      where: {
+        [Op.or]: [
+          { id: userIds },
+          { id: donorIds }
+        ]
+      },
+      attributes: ['id', 'f_name'] // Fetch only id and f_name fields
+    });
+
+    // Organize the user details into an object for easier lookup
+    const userMap = {};
+    users.forEach(user => {
+      userMap[user.id] = user;
+    });
+
+    // Combine the original data with user details
+    const enrichedData = data.map(item => {
+      return {
+        ...item.get(),
+        donorBy: { f_name: userMap[item.user_id].f_name },
+        requestedBy: { f_name: userMap[item.accepted_donor].f_name }
+      };
+    });
+
+    successResponse(200, "OK", enrichedData, res);
   } catch (err) {
     errorResponse(
       500,
--- modules/serviceRequests/collPointRequest/routes/collPointReq.route.js
+++ modules/serviceRequests/collPointRequest/routes/collPointReq.route.js
@@ -3,6 +3,6 @@ const collPointReqRouter = require("express").Router();
 const collPointReqController = require("../controllers/collPointReq.Controller.js");
 
 module.exports = (app) => {
-  collPointReqRouter.get("/:userId", collPointReqController.getAll);
+  collPointReqRouter.get("/:collectionPoint", collPointReqController.getAll);
   app.use("/api/services/coll-point-requests", collPointReqRouter);
 };
