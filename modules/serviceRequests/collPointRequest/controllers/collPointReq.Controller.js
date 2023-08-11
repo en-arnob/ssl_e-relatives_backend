d181e8a50d40f529d9036df1e63847b5ff069047
Mizanur Rahman <mizanurewu@gmail.com>
2023-08-11 10:35:07 +0600

    collPoint change

--- modules/serviceRequests/collPointRequest/controllers/collPointReq.Controller.js
+++ modules/serviceRequests/collPointRequest/controllers/collPointReq.Controller.js
@@ -1,10 +1,6 @@
 const db = require("../../../../config/database.config");
 const BloodRequest = db.model.bloodReq;
 const User = db.model.user;
-const UserDetails = db.model.UserDetails;
-const City = db.model.city;
-const State = db.model.state;
-const Country = db.model.country;
 const errorResponse = require("../../../../utils/errorResponse");
 const successResponse = require("../../../../utils/successResponse");
 const { Op } = require("sequelize");
@@ -21,43 +17,25 @@ exports.getAll = async (req, res) => {
           [Op.not]: null,
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'req_by',
+          attributes: ['f_name']
+        },
+        {
+          model: User,
+          as: 'donor',
+          attributes: ['f_name']
+        }
+      ]
     });
 
     if (data.length === 0) {
       return errorResponse(404, "NOT_FOUND", "No data found", res);
     }
 
-    // Extract user_ids and accepted_donor values from the data
-    const userIds = data.map(item => item.user_id);
-    const donorIds = data.map(item => item.accepted_donor);
-
-    // Fetch user details for user_ids and accepted_donor values
-    const users = await User.findAll({
-      where: {
-        [Op.or]: [
-          { id: userIds },
-          { id: donorIds }
-        ]
-      },
-      attributes: ['id', 'f_name'] // Fetch only id and f_name fields
-    });
-
-    // Organize the user details into an object for easier lookup
-    const userMap = {};
-    users.forEach(user => {
-      userMap[user.id] = user;
-    });
-
-    // Combine the original data with user details
-    const enrichedData = data.map(item => {
-      return {
-        ...item.get(),
-        donorBy: { f_name: userMap[item.user_id].f_name },
-        requestedBy: { f_name: userMap[item.accepted_donor].f_name }
-      };
-    });
-
-    successResponse(200, "OK", enrichedData, res);
+    successResponse(200, "OK", data, res);
   } catch (err) {
     errorResponse(
       500,
