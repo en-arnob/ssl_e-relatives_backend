bc4c2e077589ade712009a8e017ca42301151bf8
Khalid Utsob <en.arnob@gmail.com>
2023-08-28 16:10:24 +0600

    Request for Test Completed

--- modules/serviceRequests/testReq/controllers/testReq.controller.js
+++ modules/serviceRequests/testReq/controllers/testReq.controller.js
@@ -1,7 +1,70 @@
+const errorResponse = require("../../../../utils/errorResponse");
+const successResponse = require("../../../../utils/successResponse");
+require("dotenv").config();
+const db = require("../../../../config/database.config");
+const axios = require("axios");
+const TestReq = db.model.testReq;
+
 exports.create = async (req, res) => {
+  const testdata = req.body;
   try {
-    res.json({ msg: "Hello Test" });
-  } catch (error) {
-    console.log(error);
+    const reqNo = Math.floor(
+      1000000000 +
+        Math.random() *
+          9000000000 *
+          Math.floor(1000000000 + Math.random() * 9000000000 * Date.now())
+    )
+      .toString()
+      .substring(2, 10);
+    if (
+      parseInt(testdata.selectionType) === 1 &&
+      testdata.investigationArr.length > 0
+    ) {
+      const invCsv = testdata.investigationArr.join(",");
+      const newTestReq = await TestReq.create({
+        user_id: testdata.userId,
+        req_no: reqNo,
+        select_type: testdata.selectionType,
+        investigation_ids: invCsv,
+      });
+      newTestReq && successResponse(200, "OK", testdata, res);
+    } else if (parseInt(testdata.selectionType) === 2 && testdata.file) {
+      const newTestReq = await TestReq.create({
+        user_id: testdata.userId,
+        req_no: reqNo,
+        select_type: testdata.selectionType,
+        inv_image: testdata.file,
+      });
+      newTestReq && successResponse(200, "OK", testdata, res);
+    } else {
+      errorResponse(
+        500,
+        "ERROR",
+        "Some error occurred while Creating Request, Please try providing all information",
+        res
+      );
+    }
+  } catch (err) {
+    errorResponse(
+      500,
+      "ERROR",
+      err.message || "Some error occurred while Creeating Request",
+      res
+    );
+  }
+};
+
+exports.uploadInvImage = async (req, res) => {
+  try {
+    let imageFile = req.file;
+    res.send(imageFile);
+  } catch (err) {
+    errorHandler(
+      500,
+      "ERROR",
+      err.message ||
+        "Some error occurred while Finding Users By Date_of_Birth.",
+      res
+    );
   }
 };
--- modules/serviceRequests/testReq/routes/testReq.route.js
+++ modules/serviceRequests/testReq/routes/testReq.route.js
@@ -1,9 +1,17 @@
 const testReqRouter = require("express").Router();
 const testReqController = require("../controllers/testReq.controller.js");
+const invImageUploadMulter = require("../../../../middlewares/uploadInvestigationImage.js");
 
 module.exports = (app) => {
-  // e-rel new methods
+  // create test request
   testReqRouter.post("/", testReqController.create);
+
+  // upload investigation image file
+  testReqRouter.post(
+    "/upload-image",
+    invImageUploadMulter,
+    testReqController.uploadInvImage
+  );
 
   app.use("/api/services/request/test", testReqRouter);
 };
